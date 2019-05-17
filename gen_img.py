42ef417a7dcddb4a1f1274b0ea62ac3ed654e1a2
Simon Christmann <simon@christmann.email>
2019-05-17 12:32:34 +0200

    Use greyscale instead of RGB

--- gen_img.py
+++ gen_img.py
@@ -7,7 +7,7 @@ def gen_img(txt, label, img_fraction=0.8, fontname='VeraMoBd.ttf'):
     W, H = get_max_pixel_size_of_label(label)
 
     # Generate image
-    img = Image.new('RGB', (W,H), color = 'white')
+    img = Image.new('L', (W,H), color='white')
     draw = ImageDraw.Draw(img)
 
     fontsize = 1
